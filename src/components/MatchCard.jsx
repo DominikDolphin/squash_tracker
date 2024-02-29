e18103ff68ae1f6375b2d6e8ecdc60adfee886d6
Dominik Thibaudeau <dominik.thibaudeau@gmail.com>
2024-02-29 15:38:16 -0500

    Added Settings modal with basic config layout

--- src/components/MatchCard.jsx
+++ src/components/MatchCard.jsx
@@ -1,6 +1,9 @@
-import { Card, CardContent, CardHeader } from "@mui/material";
+import { Card, CardContent, CardHeader, IconButton } from "@mui/material";
+import { Settings } from "@mui/icons-material";
 import MatchTable from "./MatchTable";
 import { useState } from "react";
+import MatchSettingsModal from "./MatchSettingsModal";
+
 export default function MatchCard() {
 
   const [players, setPlayers] = useState([
@@ -8,16 +11,37 @@ export default function MatchCard() {
     { id: 2, name: "Dominik" },
   ]);
 
+  const [matchSettingsModalOpen, setMatchSettingsModalOpen] = useState(true);
+
+  const openSettingsModal = () => {
+    console.log('Settings modal opened');
+    setMatchSettingsModalOpen(true);
+  }
+
+  const closeSettingsModal = () => {
+    setMatchSettingsModalOpen(false)
+  }
+
+
   return (
-    <Card>
-      <CardHeader
-        title="🏆 Winner - Michael 🏆"
-        subheader="Best out of 3"
-        style={{ paddingBottom: "0px", textAlign: "center" }}
-      />
-      <CardContent style={{ PaddingTop: "0px" }}>
-        <MatchTable players={players}/>
-      </CardContent>
-    </Card>
+    <>
+      <Card>
+        <CardHeader
+          title="🏆 Winner - Michael 🏆"
+          subheader="Best out of 3"
+          style={{ paddingBottom: "0px", textAlign: "center" }}
+          action={
+            <IconButton aria-label="settings" onClick={openSettingsModal}>
+              <Settings />
+            </IconButton>
+          }
+        />
+        <CardContent style={{ PaddingTop: "0px" }}>
+          <MatchTable players={players} />
+        </CardContent>
+      </Card>
+
+      <MatchSettingsModal isOpen={matchSettingsModalOpen} players={players} handleCloseModal={closeSettingsModal} />
+    </>
   );
 }
